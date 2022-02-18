2488b7bf62859996e56f28cadf5f03559cd20a68
Gabriela <Gabriela@DESKTOP-MAI8I4K>
2022-02-18 16:16:13 -0300

    Add project files.

---
+++ FrequenciaNumerosConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FrequenciaNumerosConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantnumeros;
+
+            Console.WriteLine("digite a quantidade de números: ");
+            quantnumeros = int.Parse(Console.ReadLine());
+
+            int[] numeros = new int[quantnumeros];
+
+            for (int i = 0; i < quantnumeros; i++) 
+
+
+
+
+        }
+    }
+}
