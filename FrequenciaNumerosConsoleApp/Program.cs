3451becee6b4fbbf0e710d25c046c29157ba33ab
Gabriela Westphal Cordova <gabiwestphal@hotmail.com>
2022-03-01 12:55:27 -0300

    Finalizado

--- FrequenciaNumerosConsoleApp/Program.cs
+++ FrequenciaNumerosConsoleApp/Program.cs
@@ -7,14 +7,36 @@ namespace FrequenciaNumerosConsoleApp
     {
         static void Main(string[] args)
         {
-            int quantnumeros;
+            int[] array = new int[] { 8, 10, 8, 260, 4, 10, 10 };
 
-            Console.WriteLine("digite a quantidade de números: ");
-            quantnumeros = int.Parse(Console.ReadLine());
+            Array.Sort(array);
 
-            int[] numeros = new int[quantnumeros];
+            int i = 0;
+            int j = 0;
 
-            for (int i = 0; i < quantnumeros; i++) 
+            while (i < array.Length)
+            {
+                int numero = array[i];
+                int quantidadeVezes = 0;
+
+                while (array[i] == array[j])
+                {
+                    quantidadeVezes++;
+                    j++;
+
+                    if (j == array.Length) 
+                        break;
+                }
+
+                string sufixo = (quantidadeVezes > 1) ? " vezes" : " vez";
+
+                Console.WriteLine(numero + " apareceu " + quantidadeVezes + sufixo);
+
+                i = j;
+            }          
+
+            Console.ReadLine();
+
 
 
 
